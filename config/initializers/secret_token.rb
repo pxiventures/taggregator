cbeecba17dddcde9000a8d85a9ff4da803b50598
David Somers <jalada@gmail.com>
2014-07-23 15:06:33 +0100

    Ability to set secret token from an environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Taggregator::Application.config.secret_token = 'PLEASE_CHANGE_ME43cd82416fe9e3182b8de2e674ebba59611c331b643ebaf5b349032d2cd0303d'
+Taggregator::Application.config.secret_token = ENV["SECRET_TOKEN"] || 'PLEASE_CHANGE_ME43cd82416fe9e3182b8de2e674ebba59611c331b643ebaf5b349032d2cd0303d'
